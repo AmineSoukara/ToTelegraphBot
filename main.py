e67180e3ebf3d04f5e6e79c81287ca13240a4528
AmineSoukara <65272894+AmineSoukara@users.noreply.github.com>
2021-01-20 03:26:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ TGraph = Client(
 UPDATES_CHANNEL = os.environ.get('UPDATES_CHANNEL', 'DamienSoukara')
 home_text = """
 Hi, [{}](tg://user?id={})
-I Am Telegram To Telegra.ph Image Uploader Bot.
+I Am Telegra.ph Image Uploader Bot.
 
 Send Me Any Image I Will Upload It To Telegra.ph.
 """
@@ -83,7 +83,7 @@ async def start(client, message):
             return
     ##
     await message.reply_text(
-        f"Hi, {message.from_user.mention}.\nI Am Telegram To Telegra.ph Image Uploader Bot.",
+        f"Hi, {message.from_user.mention}.\nI Am Telegra.ph Image Uploader Bot.",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
